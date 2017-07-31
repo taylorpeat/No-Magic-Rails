6d54d97b89369b584882f9e52e185941bed3e7c1
Taylor Peat <taylorpeat@hotmail.com>
2017-07-31 13:55:11 -0400

    initial commit

--- Gemfile
+++ Gemfile
@@ -34,6 +34,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 group :development, :test do
   gem 'pry'
+  gem 'rb-readline'
 
   # Access an IRB console on exception pages or by using <%= console %> in views
   gem 'web-console', '~> 2.0'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  def list_posts
+    connection = SQLite3::Database.new 'db/development.sqlite3'
+    connection.results_as_hash = true
+
+    posts = connection.execute("SELECT * FROM posts")
+
+    render 'application/list_posts', locals: { posts: posts }
+  end
 end
