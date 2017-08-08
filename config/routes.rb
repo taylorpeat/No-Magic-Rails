61f750d695909749f4353fefdb80ea4cd936abd5
Taylor Peat <taylorpeat@hotmail.com>
2017-08-08 09:13:44 -0400

    routes and resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 Rails.application.routes.draw do
-  get '/posts' => 'application#list_posts'
-  get '/posts/:id' => 'application#show_post'
-  get '/post/new' => 'application#new_post'
-  get '/posts/:id/edit' => 'application#edit_post'
-  get  '/comments' => 'application#list_comments'
-  post '/posts' => 'application#create_post'
-  patch '/posts/:id' => 'application#update_post'
-  delete '/posts/:id' => 'application#delete_post'
-  post '/posts/:post_id/comments' => 'application#create_comment'
-  delete '/posts/:post_id/comments/:id' => 'application#delete_comment'
+  resources :posts do
+    resources :comments, only: [:create, :destroy]
+  end
+
+  resources :comments, only: :index
 end
