4d6bcf943cc8cd1eb8630612c77d989c4f331794
Taylor Peat <taylorpeat@hotmail.com>
2017-08-11 23:14:37 -0400

    Application helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,34 @@
 module ApplicationHelper
+
+  def my_hidden_field_tag(name, value)
+    "<input type='hidden' name='#{name}' value='#{value}' >".html_safe
+  end
+
+  def my_label_tag(txt)
+    "<label>#{txt}</label>".html_safe
+  end
+
+  def my_text_field_tag(name, value)
+    "<input name='#{name}' value='#{value}' type=text >".html_safe
+  end
+
+  def my_text_area_tag(name, value)
+    "<textarea name='#{name}' value='#{value}'></textarea>".html_safe
+  end
+
+  def my_submit_tag(txt="Submit")
+    "<input type='submit' value='#{txt}'>".html_safe
+  end
+
+  def my_form_tag(path, &block)
+    attrs = "method='post' action='#{path}'"
+    fields = capture(&block)
+    "<form #{attrs}> #{my_authenticity_token_field} #{fields} </form>".html_safe
+  end
+
+  def my_authenticity_token_field
+    my_hidden_field_tag('authenticity_token', form_authenticity_token)
+  end
+
 end
+
